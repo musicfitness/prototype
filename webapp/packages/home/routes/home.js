2b695c88da38f1472cd7376ffa23686867091913
Tom Brückner <tom.brueckner@gmx.net>
2016-04-22 21:33:34 +0200

    removed trigger

--- webapp/packages/home/routes/home.js
+++ webapp/packages/home/routes/home.js
@@ -1,8 +1,5 @@
 FlowRouter.route('/', {
   name: 'home',
-  // triggersEnter: [function(context, redirect) {
-  //   redirect('/map');
-  // }],
   action: function() {
     var currentLayout = Session.get('currentLayout');
     var mainCanvasContent = 'home';
