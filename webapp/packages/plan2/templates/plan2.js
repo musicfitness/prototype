78d55a7a94dd47645a5bc7a1d61e062c3fa35171
JonasJustus <travel.florian@gmail.com>
2016-04-23 11:39:59 +0200

    FrontEnd Navigation Settings

--- webapp/packages/plan2/templates/plan2.js
+++ webapp/packages/plan2/templates/plan2.js
@@ -1,1 +1,10 @@
-
+Template.plan2.events({
+  'click .js-button-situp': () => {
+    FlowRouter.go('plan3');
+  }
+});
+Template.plan2.events({
+'click .js-button-plan': () => {
+  FlowRouter.go('plan');
+}
+});
--- webapp/packages/plan3/templates/plan3.js
+++ webapp/packages/plan3/templates/plan3.js
@@ -1,1 +1,5 @@
-
+Template.plan3.events({
+  'click .js-button-plan2': () => {
+    FlowRouter.go('plan2');
+  }
+});
